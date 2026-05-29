018d82a02a0952e9c47375cdbecfee9e8f5491e1
Nishka Batra <niksbatra09@gmail.com>
2026-05-29 09:13:48 -0700

    Update pbkdf2_demo.py

--- pbkdf2_demo.py
+++ pbkdf2_demo.py
@@ -2,24 +2,14 @@ from hashlib import pbkdf2_hmac
 import secrets
 
 # PBKDF2-HMAC-SHA512 password hashing demo
-
 salt = secrets.token_bytes(16)
-
 password = input("Password: ")
-
 key = pbkdf2_hmac("sha512", password.encode("utf-8"), salt, 100_000)
 
 print("Salt:", salt.hex())
 print("Hash:", key.hex())
 
 # simple login verification
-attempt = input("Enter password again: ")
-
-attempt_key = pbkdf2_hmac(
-    "sha512",
-    attempt.encode("utf-8"),
-    salt,
-    100_000
-)
-
-print("Match:", attempt_key == key)
+guess = input("Enter password again: ")
+guess_key = pbkdf2_hmac("sha512", guess.encode("utf-8"), salt, 100_000)
+print("Match:", guess_key == key)
